ac86806afa06c3c9687eaa2117a024947785ec43
Adolfo <adolfo.rivas@Lennar.com>
2023-06-19 11:49:22 +0200

    navabar
    navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Pagination from "./components/Pagination";
+import React, { useEffect, useRef, useState } from "react";
 function App() {
+  const links = [{ id: 1 }];
+  let [num, setNum] = useState(1);
+  let [cur, setCur] = useState(1);
 
-  const links = [
-    {id:1},
+  const pages = [
+    { page: num },
+    { page: num + 1 },
+    { page: num + 2 },
+    { page: num + 3 },
   ];
-
-
+  function Next() {
+    setNum(++num);
+  }
+  function back() {
+    num > 1 && setNum(--num);
+  }
   return (
-    <div className='w-full h20 text-3xl font-bold flex justify-center items-center px-4 py-16	' >
-      <h1 className='text-5xl '>farmacias Zacatelco</h1>
+    <div className="w-full h20 text-3xl font-bold flex justify-center items-center px-4 py-16	">
+      <h1 className="text-5xl ">farmacias Zacatelco</h1>
       <div>
         <ul>
-          <li>
-
-          </li>
+          <li></li>
         </ul>
       </div>
-      
-    </div>
 
+      <Pagination></Pagination>
+
+      <div class="flex flex-col items-center">
+        <span class="text-sm text-gray-700 dark:text-gray-400">
+          Showing{" "}
+          <span class="font-semibold text-gray-900 dark:text-white">1</span> to{" "}
+          <span class="font-semibold text-gray-900 dark:text-white">10</span> of{" "}
+          <span class="font-semibold text-gray-900 dark:text-white">100</span>{" "}
+          Entries
+        </span>
+        <div class="inline-flex mt-2 xs:mt-0">
+          <button class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 mr-2"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            Prev
+          </button>
+          <button class="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-gray-800 border-0 border-l border-gray-700 rounded-r hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+            Next
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 ml-2"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
