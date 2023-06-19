8052d2fd51fb80f7d437df8498e71862d683a7dc
Adolfo <adolfo.rivas@Lennar.com>
2023-06-19 11:32:21 +0200

    tailwindnavbar
    tailwindnavbar

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const links = [
+    {id:1},
+  ];
+
+
+  return (
+    <div className='w-full h20 text-3xl font-bold flex justify-center items-center px-4 py-16	' >
+      <h1 className='text-5xl '>farmacias Zacatelco</h1>
+      <div>
+        <ul>
+          <li>
+
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+
+  );
+}
+
+export default App;
